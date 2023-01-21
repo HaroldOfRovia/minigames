5ea02e68bed26c180492e57c3fd8f92e7c438d3b
Илья Кудинкин <kudincikin@gmail.com>
2023-01-21 21:02:18 +0300

    создано игровое поле, клеточки теперь содержат бомбы и цифры, клеточки изначально закрыты

--- src/components/minesweeper/Cell.tsx
+++ src/components/minesweeper/Cell.tsx
@@ -1,13 +1,36 @@
-import React from "react"
+import React, { CSSProperties, useState } from "react"
 import '../../styles/minesweeper/Cell.css';
 
 export interface MineCell{
     x: number;
     y: number;
+    content: string | number;
+    open: boolean
 }
 
-export const Cell = ({ x, y }: MineCell) => {
+export const Cell = ({ x, y, content, open }: MineCell) => {
+    const [show, setShow] = useState(open);
+
+    document.getElementById(x + "-" + y)?.addEventListener('click', (e) =>{
+        setShow(true);
+    });
+
+    if(show){
+        if(content === 'bomb'){
+            return (
+                <div className="cell" id={x + "-" + y} style={{ backgroundColor: 'black' }}/>
+            )
+        }
+        else if(typeof content === 'number'){
+            return (
+                <div className={`cell num${content}`} id={x + "-" + y}>{ content }</div>
+            )
+        }
+        return(
+            <div className="cell" id={x + "-" + y}>-</div>
+            )
+    }
     return(
-        <div className="closedCell" id={x + "-" + y}/>
+        <div className="cell" id={x + "-" + y}/>
     )
 }
--- src/components/minesweeper/Minesweeper.tsx
+++ src/components/minesweeper/Minesweeper.tsx
@@ -3,47 +3,100 @@ import { Cell, MineCell } from "./Cell"
 import '../../styles/minesweeper/Minesweeper.css'
 
 export const Minesweeper = () => {
-    let generated = false;
-    const [width, setWidth] = useState(25);
-    const [height, setHeight] = useState(25);
-    const [cells, setCells] = useState<MineCell[][]>();
+    let generated = false, 
+        width = 10,
+        height = 10,
+        bombs = Math.floor((width*height) / 4);
+    const [cells, setCells] = useState<MineCell[][]>(initCells);
 
-    function initCells(){
+    function initCells(): MineCell[][]{
         let arr: MineCell[][] = [];
         for(let i = 0; i < width; i++){
             arr.push([]);
             for(let j = 0; j < height; j++)
-                arr[i].push({ x: i, y: j});
+                arr[i].push({ x: i, y: j, content: "", open: false });
         }
-        setCells(arr);
+        return arr;
     }
 
-    function generateGame(){
-
+    function setMines(arr: MineCell[][], clickX: number, clickY: number){
+        let placedBombs = 0;
+        for(let i = 0; placedBombs < bombs; i++){
+            let x = Math.floor(Math.random() * width),
+                y = Math.floor(Math.random() * height);
+            if(clickX === x && clickY === y)
+                continue;
+            if(arr[x][y].content !== 'bomb'){
+                arr[x][y].content = 'bomb';
+                placedBombs++;
+            }
+        }
     }
 
-    document.getElementById('field')?.addEventListener('click', (e) => {
-        const el = e.target as HTMLElement;
-        console.log(el);
-    })
+    function getNumCell(arr: MineCell[][], x: number, y: number){
+        let count = 0;
+        for (let i = y - 1; i < y + 2 && i < arr.length; i++){
+            for (let j = x - 1; j < x + 2 && j < arr[0].length; j++){
+                if (i < 0 || j < 0)
+                    continue;
+                if (arr[i][j].content === "bomb")
+                    count++;
+            }
+        }
+        return count;
+    }
+
+    function setNumbers(arr: MineCell[][]){
+        for(let i = 0; i < height; i++){
+            for(let j = 0; j < width; j++){
+                if(arr[i][j].content === 'bomb')
+                    continue;
+                let num = getNumCell(arr, j, i);
+                if(num)
+                    arr[i][j].content = num;
+            }
+        }
+    }
+
+    function generateGame(arr: MineCell[][], clickX: number, clickY: number){
+        generated = true;
+        setMines(arr, clickX, clickY);
+        setNumbers(arr)
+        
+        setCells([...arr]);
+    }   
 
     useEffect(()=>{
-        initCells();
+        document.getElementById('field')?.addEventListener('click', (e) => {
+            if(generated)
+                return;
+            const el = e.target as HTMLElement;
+            let [x, y] = el.id.split("-");
+            generateGame(cells, +x, +y);
+        }, true)
     }, []);
 
     return (
-        <div id="field" style={ cssField(width, height) }>
-            { 
-            (function(){
-                if(!cells)
-                    return;
-                return cells.map((row) => {
-                    return row.map((el) => {
-                        return <Cell key={el.x + "-" + el.y} {...el} />});
-                    });
-                }())
-            }
-        </div>
+        <>
+            <form id="gameSettings">
+                <input placeholder="высота поля"></input>
+                <input placeholder="ширина поля"></input>
+                <input placeholder="кол-во мин"></input>
+                <button>Обновить</button>
+            </form>
+            <div id="field" style={ cssField(width, height) }>
+                { 
+                (function(){
+                    if(!cells)
+                        return;
+                    return cells.map((row) => {
+                        return row.map((el) => {
+                            return <Cell key={el.x + "-" + el.y} {...el} />});
+                        });
+                    }())
+                }
+            </div>
+        </>
     )
 }
 
@@ -54,3 +107,10 @@ function cssField(width: number, height: number){
     }
     return field;
 }
+
+function shuffle(array: any[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
